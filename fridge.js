792671c75478b9eade3720b8e636bfbf42ded920
david <david@watermelonfueled.com>
2018-02-08 15:30:47 -0500

    init commit

---
+++ fridge.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  // open/close door
+  $('.door').on('click', function() {
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open');
+    } else {
+      $(this).addClass('open');
+    }
+  });
+
+  function replaceFreezer() {
+
+  }
+
+  function replaceFridge() {
+
+  }
+
+  function replaceFreezerItem(x,y,z) {
+
+  }
+
+  function replaceFridgeItem(x,y,z) {
+
+  }
+});
